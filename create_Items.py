26caa708648100c66a425bbd3461de6c4c5e03ff
Julia <73357213+savelievayuliia@users.noreply.github.com>
2020-11-29 11:46:58 +0200

    chapter10_task05

---
+++ create_Items.py
@@ -0,0 +1,9 @@
+from ua.univer.lesson.lesson06.retail_item.RetailItem_class import RetailItem
+
+item1 = RetailItem("Пиджак", "12", "59.95")
+item2 = RetailItem("Дизайнерские джинсы", "40", "34.95")
+item3 = RetailItem("Рубашка", "20", "24.95")
+
+print(item1.get_all())
+print(item2.get_all())
+print(item3.get_all())
